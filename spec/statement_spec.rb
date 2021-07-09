2f953af4ecf4abb83a36faa74849cd271732fb7c
charlie-casserly <charliecasserly215@gmail.com>
2021-07-09 14:46:04 +0100

    #current_balance test written

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,7 +3,7 @@ require 'statement'
 describe Statement do
   let(:statement) { described_class.new }
 
-  context '#record_transaction' do
+  describe '#record_transaction' do
     before do
       statement.record_transaction(50)
     end
@@ -12,7 +12,7 @@ describe Statement do
       expect(statement.log.first[:date]).to be_a(DateTime)
     end
 
-    it 'logs the amount of the deposit' do
+    it 'logs the amount of a transaction' do
       expect(statement.log.first[:transaction]).to eq(50)
     end
 
@@ -21,22 +21,14 @@ describe Statement do
     end
   end
 
-  context '#record_withdraw' do
-    before do
+  describe '#current_balance' do 
+    it 'sums the history of all transactions in the log' do 
+      statement.record_transaction(50)
+      statement.record_transaction(60)
       statement.record_transaction(100)
-      statement.record_transaction(-50)
-    end
+      statement.record_transaction(-20)
 
-    it 'logs a date for a withdrawal' do
-      expect(statement.log[1][:date]).to be_a(DateTime)
-    end
-
-    it 'logs the amount of the withdrawal' do
-      expect(statement.log[1][:transaction]).to eq(-50)
-    end
-
-    it 'logs the balance after a withdrawal' do
-      expect(statement.log[1][:balance]).to eq(50)
+      expect(statement.current_balance).to eq(190)
     end
   end
 end
