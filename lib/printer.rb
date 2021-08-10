581505ac1d383f4dcc78619077054a8764b24cdf
charlie-casserly <charliecasserly215@gmail.com>
2021-08-10 20:00:44 +0100

    \n fixed in terminal

--- lib/printer.rb
+++ lib/printer.rb
@@ -23,11 +23,11 @@ class Printer
     end
 
     def format_credit(transaction)
-      "#{format_currency(transaction[:transaction])}  ||       "
+      "#{format_currency(transaction[:transaction])}  ||      "
     end
 
     def format_debit(transaction)
-      " || #{format_currency(transaction[:transaction].to_i.abs)} "
+      "       || #{format_currency(transaction[:transaction].to_i.abs)}"
     end
 
     def format_currency(amount)
